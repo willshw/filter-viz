3582b3e97f14ab0ba768fc5721b066aa2f52a506
William Wang <shanhewang@gmail.com>
2018-11-15 01:53:09 -0600

    added tails to points

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 import sys
 import time
 import pyautogui
-
+from Queue import Queue
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
-# pg.setConfigOption('background', 'w')
+pg.setConfigOption('background', 'w')
 
 import kalman_filtered_mouse as kfm
 
@@ -17,6 +17,11 @@ class Filter:
 
     def __init__(self, filter_type):
         self.mouse_pos = [0, 0]
+
+        self.meas_Xx = Queue()
+        self.meas_Xy = Queue()
+        self.pred_Xx = Queue()
+        self.pred_Xy = Queue()
 
         if filter_type == 'kalman_filter':
             self._filter = kfm.KalmanFilteredMouse()
@@ -36,11 +41,29 @@ class Filter:
 
     def getMeasuredStates(self):
 
-        return self._filter.getMeasuredStates()
+        Y = self._filter.getMeasuredStates()
+
+        self.meas_Xx.put(Y[0,0])
+        self.meas_Xy.put(Y[1,0])
+    
+        if self.meas_Xx.qsize() > 10:
+            self.meas_Xx.get([0])
+            self.meas_Xy.get([0])
+
+        return list(self.meas_Xx.queue), list(self.meas_Xy.queue)
 
     def getPredictiedStates(self):
+        
+        X = self._filter.getPredictiedStates()
 
-        return self._filter.getPredictiedStates()
+        self.pred_Xx.put(X[0,0])
+        self.pred_Xy.put(X[1,0])
+
+        if self.pred_Xx.qsize() > 10:
+            self.pred_Xx.get([0])
+            self.pred_Xy.get([0])
+
+        return list(self.pred_Xx.queue), list(self.pred_Xy.queue)
 
 class Plot:
     
@@ -49,12 +72,17 @@ class Plot:
         self._p = self._window.addPlot()
         self._p.enableAutoRange("xy", False)
         self._p.hideButtons()
-        self._p.setXRange(0, 1920)
-        self._p.setYRange(0, 1080)
-        self._p.getViewBox().invertY(True)
-
-        self._plot_measure = self._p.plot([], [], pen=None, symbol='o', symbolBrush=(255, 50, 50, 100))
-        self._plot_predict = self._p.plot([], [], pen=None, symbol='o', symbolBrush=(50, 50, 255, 100))
+        self._p.setXRange(0, 2000, 0)
+        self._p.setYRange(0, 2000, 0)
+        # self._p.getViewBox().invertY(True)
+        
+        self._plot_measure = []
+        self._plot_predict = []
+        for i in range(0,10,1):
+            measure_symbol_brush = pg.mkBrush((255, 25, 25, i*10))
+            predict_symbol_brush = pg.mkBrush((25, 25, 255, i*10))
+            self._plot_measure.append(self._p.plot([], [], pen=None, symbol='o', symbolBrush=measure_symbol_brush))
+            self._plot_predict.append(self._p.plot([], [], pen=None, symbol='o', symbolBrush=predict_symbol_brush))
 
         self.mouse_pos = [0, 0]
 
@@ -74,10 +102,12 @@ class Plot:
         return self.mouse_pos
 
     def setMeasureData(self, x, y):
-        self._plot_measure.setData(x, y)
+        for i in range(len(x)):
+            self._plot_measure[i].setData([x[i]], [y[i]])
     
     def setPredictData(self, x, y):
-        self._plot_predict.setData(x, y)
+        for i in range(len(x)):
+            self._plot_predict[i].setData([x[i]], [y[i]])
 
 def main():
 
@@ -93,14 +123,15 @@ def main():
             kf.setMousePos(plt.getMousePos())
             kf.runFilter()
 
-            Y = kf.getMeasuredStates()
-            X = kf.getPredictiedStates()
+            Yx, Yy = kf.getMeasuredStates()
+            Xx, Xy = kf.getPredictiedStates()
 
-            plt.setMeasureData([int(Y[0,0])], [int(Y[1,0])])
-            plt.setPredictData([X[0,0]], [X[1,0]])
+            plt.setMeasureData(Yx, Yy)
+            plt.setPredictData(Xx, Xy)
+
             QtGui.QApplication.processEvents()
 
-            positionStr = 'X: {0:d} Y: {1:d} Xp: {2:.2f} Yp: {3:.2f}'.format(int(Y[0,0]), int(Y[1,0]), X[0,0], X[1,0])
+            positionStr = 'X: {0:.2f} Y: {1:.2f} Xp: {2:.2f} Yp: {3:.2f}'.format(Yx[-1], Yy[-1], Xx[-1], Xy[-1])
             print positionStr,
             print '\b' * (len(positionStr) + 2),
             sys.stdout.flush()
