19b0e0e7ef4968870bdca54dfa3908b050daa9bc
William Wang <shanhewang@gmail.com>
2019-01-02 00:26:48 -0600

    finished GUI layout

---
+++ filterLib.py
@@ -0,0 +1,147 @@
+import numpy as np
+import copy
+
+# Filter parameters container class for better organization of parameters
+class FilterParameter:
+
+    def __init__(self, name, data, doc=None):
+        self._name = copy.deepcopy(name)
+        self._data = copy.deepcopy(data)
+        self._doc = copy.deepcopy(doc)
+    
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def data(self):
+        return self._data
+    
+    @data.setter
+    def data(self, d):
+        self._data = copy.deepcopy(d)
+        print 'Set Parameter {0} to {1}'.format(self._name, d) 
+
+    @property
+    def doc(self):
+        return self._doc
+
+# KalmanFilter class handles all the calculation in the kalman filter
+class KalmanFilter:
+
+    def __init__(self):
+        """
+        states of mouse pointer: [x, y, dx, dy]
+        
+        State Transition Matrix:
+        A = [[1, 0, 0.25, 0],
+             [0, 1, 0, 0.25],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
+
+        Control Matrix:
+        B = indentity(4)
+
+        Measurement Matrix:
+        C = [[1, 0, 1, 0],
+             [0, 1, 0, 1],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
+
+        Action Uncertainty Matrix:
+        Q = [[0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0.1, 0],
+             [0, 0, 0, 0.1]]
+
+        Sensor Noise Matrix:
+        R = [[0.1, 0, 0, 0],
+             [0, 0.1, 0, 0],
+             [0, 0, 0.1, 0],
+             [0, 0, 0, 0.1]]
+        
+        """
+        self._params = {}
+        self._params.update({'A': FilterParameter('A', np.identity(4), 'State Transition Matrix')})
+        self._params['A'].data[0, 2] = 0.25
+        self._params['A'].data[1, 3] = 0.25
+
+        self._params.update({'B': FilterParameter('B', np.identity(4), 'Control Matrix')})
+
+        self._params.update({'C': FilterParameter('C', np.identity(4), 'Measurement Matrix')})
+        self._params['C'].data[0, 2] = 1.0
+        self._params['C'].data[1, 3] = 1.0
+
+        self._params.update({'Q': FilterParameter('Q', np.zeros((4, 4)), 'Action Uncertainty Matrix')})
+        self._params['Q'].data[2, 2] = 0.1
+        self._params['Q'].data[3, 3] = 0.1
+
+        self._params.update({'R': FilterParameter('R', np.identity(4)*0.1, 'Sensor Noise Matrix')})
+        
+        # state
+        self._X_curr = np.zeros((4, 1))
+        self._X_prev = np.zeros((4, 1))
+        self._X_pred = np.zeros((4, 1))
+
+        # uncertainty
+        self._P_curr = np.zeros((4, 4))
+        self._P_prev = np.zeros((4, 4))
+        self._P_pred = np.zeros((4, 4))
+
+        # measurement
+        self._Y_curr = np.zeros((4, 1))
+
+        # Kalman gain
+        self._K = np.zeros((4, 4))
+
+        self._time_curr = 0
+        self._time_prev = 0
+
+    def getParams(self):
+        return self._params
+    
+    def setParams(self, n, val):
+        self._params[n].data = val
+
+    def getPredictiedStates(self):
+        return self._X_curr
+
+    def getMeasuredStates(self):
+        return self._Y_curr
+
+    def updateMeasurements(self, x, y, time):
+        """
+        Function updates the mouse state measurement, in this case just reading mouse position from PyAuotGUI
+        """
+        
+        self._time_curr = time
+        d_time = self._time_curr - self._time_prev
+
+        d_x = (x - self._Y_curr[0,0]) / d_time
+        d_y = (y - self._Y_curr[1,0]) / d_time
+
+        self._Y_curr[0,0] = x
+        self._Y_curr[1,0] = y
+        self._Y_curr[2,0] = d_x
+        self._Y_curr[3,0] = d_y
+
+    def predict(self, U= np.zeros((4, 1))):
+        """
+        Predict states and uncertainty based on the previous state and motion model
+        """
+
+        self._X_prev = self._X_curr
+        self._P_prev = self._P_curr
+
+        self._X_pred = self._params['A'].data.dot(self._X_prev) + self._params['B'].data.dot(U)
+        self._P_pred = self._params['A'].data.dot(self._P_prev).dot(self._params['A'].data.T) + self._params['Q'].data
+    
+    def update(self):
+        """
+        Update the predicted state based of the measurement
+        """
+
+        self._K = self._P_pred.dot(self._params['C'].data.T).dot(np.linalg.inv(self._params['C'].data.dot(self._P_pred).dot(self._params['C'].data.T) + self._params['R'].data))
+        
+        self._X_curr = self._X_pred + self._K.dot(self._Y_curr - self._params['C'].data.dot(self._X_pred))
+        self._P_curr = (np.identity(4) - self._K.dot(self._params['C'].data)).dot(self._P_pred)
--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 pg.setConfigOption('background', 'w')
 
-import kalman_filtered_mouse as kfm
+import numpy as np
+
+import filterLib as flib
 
 REFRESH_RATE = 30.0
 FITLER_TYPE = 'kalman_filter'
@@ -18,6 +20,8 @@ class Filter:
 
     def __init__(self, filter_type):
         self._mouse_pos = [0, 0]
+        self._mouse_pos_noise_x_std = 50.0
+        self._mouse_pos_noise_y_std = 50.0
 
         self._meas_hist_len = 3
         self._pred_hist_len = 7
@@ -28,7 +32,7 @@ class Filter:
         self._pred_Xy = Queue()
 
         if filter_type == 'kalman_filter':
-            self._filter = kfm.KalmanFilteredMouse()
+            self._filter = flib.KalmanFilter()
 
         else:
             print 'Filter type not found!'
@@ -43,6 +47,17 @@ class Filter:
     def setMousePos(self, pos):
         self._mouse_pos = pos
 
+    def setXNoiseStd(self, v):
+        self._mouse_pos_noise_x_std = v
+    
+    def setYNoiseStd(self, v):
+        self._mouse_pos_noise_y_std = v
+
+    def setMousePos_Noise(self, pos):
+        x_noise = np.random.normal(pos[0], self._mouse_pos_noise_x_std, 1)
+        y_noise = np.random.normal(pos[1], self._mouse_pos_noise_y_std, 1)
+        self._mouse_pos = [x_noise, y_noise]
+
     def setMeasHistLen(self, leng):
         leng = max(1, min(leng, 10))
         self._meas_hist_len = leng
@@ -51,6 +66,12 @@ class Filter:
         leng = max(1, min(leng, 10))
         self._pred_hist_len = leng
 
+    def getMeasHistLen(self):
+        return self._meas_hist_len
+
+    def getPredHistLen(self):
+        return self._pred_hist_len
+
     def getMeasuredStates(self):
 
         Y = self._filter.getMeasuredStates()
@@ -58,7 +79,7 @@ class Filter:
         self._meas_Xx.put(Y[0,0])
         self._meas_Xy.put(Y[1,0])
     
-        if self._meas_Xx.qsize() > self._meas_hist_len:
+        while self._meas_Xx.qsize() > self._meas_hist_len:
             self._meas_Xx.get([0])
             self._meas_Xy.get([0])
 
@@ -71,22 +92,47 @@ class Filter:
         self._pred_Xx.put(X[0,0])
         self._pred_Xy.put(X[1,0])
 
-        if self._pred_Xx.qsize() > self._pred_hist_len:
+        while self._pred_Xx.qsize() > self._pred_hist_len:
             self._pred_Xx.get([0])
             self._pred_Xy.get([0])
 
         return list(self._pred_Xx.queue), list(self._pred_Xy.queue)
 
+    def getFilterParams(self):
+        return self._filter.getParams()
+
+    def setFilterParams(self, n, val):
+        self._filter.setParams(n, val)
+
 class Plot:
     
     def __init__(self):
         self._p = pg.PlotWidget()
-        self._p.setAspectLocked()
-        self._p.setLimits(xMin=0, xMax=2000, yMin=0, yMax=2000)
-        self._p.setXRange(0, 2000, 0)
-        self._p.setYRange(0, 2000, 0)
-        self._p.hideButtons()
+
+        self._t_Meas = pg.TextItem(text='Text', color='r', anchor=(0.0, 1.5))
+        self._t_Pred = pg.TextItem(text='Text', color='b', anchor=(0.0, 1.0))
+        font = QtGui.QFont('Monospace', 8)
+        font.setStyleHint(QtGui.QFont.TypeWriter)
+        self._t_Meas.setFont(font)
+        self._t_Pred.setFont(font)
+        self._p.addItem(self._t_Meas)
+        self._p.addItem(self._t_Pred)
+
+        self._p.setAspectLocked(True)
+        self._p.setMinimumSize(300, 300)
+        self._p.setMaximumSize(1000, 1000)
         
+        self._p.setLimits(xMin=0, xMax=2000, yMin=0, yMax=2000)
+        self._p.setXRange(0, 2000, padding=0.0, update=False)
+        self._p.setYRange(0, 2000, padding=0.0, update=False)
+        self._p.disableAutoRange()
+        self._p.hideButtons()
+
+        self._p.plotItem.setClipToView(True)
+        self._p.plotItem.hideAxis('left')
+        self._p.plotItem.hideAxis('bottom')
+        self._p.plotItem.setAspectLocked()
+
         self._plot_measure = []
         self._plot_predict = []
         self._mouse_pos = [0, 0]
@@ -100,6 +146,14 @@ class Plot:
         self._timer = QtCore.QTimer()
         self._timer.timeout.connect(self.updatePlot)
         self._timer.start(0)
+
+    @property
+    def MeasStatText(self):
+        return self._t_Meas
+
+    @property
+    def PredStatText(self):
+        return self._t_Pred
 
     def getPlot(self):
         return self._p
@@ -153,10 +207,13 @@ class Plot:
         self.plotMeasuredData(self._measX, self._measY)
         self.plotPredictedData(self._predX, self._predY)
 
-        positionStr = 'X: {0:6.2f} Y: {1:6.2f} Xp: {2:6.2f} Yp: {3:6.2f}'.format(self._measX[-1], self._measY[-1], self._predX[-1], self._predY[-1])
-        print positionStr,
-        print '\b' * (len(positionStr) + 2),
-        sys.stdout.flush()
+        self._t_Meas.setText('Measurement [X:{0:=8.2f}, Y:{1:=8.2f}]'.format(self._measX[-1], self._measY[-1]))
+        self._t_Pred.setText('Prediction  [X:{0:=8.2f}, Y:{1:=8.2f}]'.format(self._predX[-1], self._predY[-1]))
+
+        # positionStr = 'X: {0:6.2f} Y: {1:6.2f} Xp: {2:6.2f} Yp: {3:6.2f}'.format(self._measX[-1], self._measY[-1], self._predX[-1], self._predY[-1])
+        # print positionStr,
+        # print '\b' * (len(positionStr) + 2),
+        # sys.stdout.flush()
 
 class FilterUpdater(QtCore.QObject):
     def __init__(self, plot, fltr):
@@ -169,7 +226,8 @@ class FilterUpdater(QtCore.QObject):
     def update(self):
         while self._isRunning:
 
-            self._fltr.setMousePos(self._plot.getMousePos())
+            # self._fltr.setMousePos(self._plot.getMousePos())
+            self._fltr.setMousePos_Noise(self._plot.getMousePos())
             self._fltr.runFilter()
 
             measX, measY = self._fltr.getMeasuredStates()
@@ -189,12 +247,13 @@ class App(QtGui.QWidget):
         super(App, self).__init__()
         self._QUIT = False
         self._plt = plot
-        self._title = 'Filtering Test'
+        self._filter = fltr
+        self._title = 'Filter Test'
         self._left = 0
         self._top = 0
-        self._width = 1000
-        self._height = 1000
-        self._initUI()
+        self._width = 500
+        self._height = 500
+        self.initUI()
 
         self._filter_updater = FilterUpdater(plot, fltr)
         self._thread = QtCore.QThread(self)
@@ -202,51 +261,207 @@ class App(QtGui.QWidget):
         self._thread.started.connect(self._filter_updater.update)
         self._thread.start()
 
-    def stopThread(self):
-        self._filter_updater.stop()
-        self._thread.quit()
-        self._thread.wait()
-
-    def _initUI(self):
+    def initUI(self):
         # configure window
         self.setWindowTitle(self._title)
         self.setGeometry(self._left, self._top, self._width, self._height)
 
         # configure widgets
 
+        '''
+        Update Button and Cancel Button
+        '''
+
         # Update Button
-        self._PushButton_update = QtGui.QPushButton('Update', self)
-        # self.PushButton_update.resize(80, 30)
-        # self.PushButton_update.move(30, 0)
+        self._PushButton_update = QtGui.QPushButton('Update Parameters', self)
         self._PushButton_update.setToolTip('Update parameters')
-        self._PushButton_update.clicked.connect(self._updateParam)
+        self._PushButton_update.clicked.connect(self.updateParams)
 
         # Quit Button
         self._PushButton_quit = QtGui.QPushButton('Quit', self)
-        # self.PushButton_quit.resize(80, 30)
-        # self.PushButton_quit.move(100, 0)
         self._PushButton_quit.setToolTip('Quit this application')
-        self._PushButton_quit.clicked.connect(self._quitApp)
+        self._PushButton_quit.clicked.connect(self.quitApp)
+
+        # Layout for Update and Quit Button
+        self._UpdateCancelButtonLayout = QtGui.QVBoxLayout()
+        self._UpdateCancelButtonLayout.addWidget(self._PushButton_update)
+        self._UpdateCancelButtonLayout.addWidget(self._PushButton_quit)
+
+        '''
+        Spin Box for Adjusting Measurement and Prediction History Display Length
+        '''
+
+        # Spin Box for Measurement History Length
+        self._Label_measHistLen = QtGui.QLabel('Measurement History Length')
+        self._Label_measHistLen.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._SpinBox_measHistLen = QtGui.QSpinBox()
+        self._SpinBox_measHistLen.setRange(1, 10)
+        self._SpinBox_measHistLen.setSingleStep(1)
+        self._SpinBox_measHistLen.setValue(self._filter.getMeasHistLen())
+        self._SpinBox_measHistLen.valueChanged.connect(self.updateMeasHistLen)
+
+        # Spin Box for Prediction History Length
+        self._Label_predHistLen = QtGui.QLabel('Prediction History Length')
+        self._Label_predHistLen.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._SpinBox_predHistLen = QtGui.QSpinBox()
+        self._SpinBox_predHistLen.setRange(1, 10)
+        self._SpinBox_predHistLen.setSingleStep(1)
+        self._SpinBox_predHistLen.setValue(self._filter.getPredHistLen())
+        self._SpinBox_predHistLen.valueChanged.connect(self.updatePredHistLen)
+
+        # Layout for Prediction History Control
+        self._HistCtrlLayout = QtGui.QVBoxLayout()
+        self._HistCtrlLayout.addWidget(self._Label_measHistLen)
+        self._HistCtrlLayout.addWidget(self._SpinBox_measHistLen)
+        self._HistCtrlLayout.addWidget(self._Label_predHistLen)
+        self._HistCtrlLayout.addWidget(self._SpinBox_predHistLen)
+
+        '''
+        Slider Bar for Adjusting Mouse X and Y Position Noise Std
+        '''
+
+        # Spin Box for X Noise
+        self._Label_xNoise = QtGui.QLabel('X Noise Std')
+        self._Label_xNoise.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._Label_xNoiseVal = QtGui.QLabel(str(50.0))
+        self._Label_xNoiseVal.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._Slider_xNoise = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self._Slider_xNoise.setMinimum(0.0)
+        self._Slider_xNoise.setMaximum(150.0)
+        self._Slider_xNoise.setValue(50.0)
+        self._Slider_xNoise.setTickInterval(1.0)
+        self._Slider_xNoise.valueChanged.connect(self.updateXNoiseStd)
+        self._filter.setXNoiseStd(50.0)
+
+        # Spin Box for Y Noise
+        self._Label_yNoise = QtGui.QLabel('Y Noise Std')
+        self._Label_yNoise.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._Label_yNoiseVal = QtGui.QLabel(str(50.0))
+        self._Label_yNoiseVal.setAlignment(QtCore.Qt.AlignCenter)
+
+        self._Slider_yNoise = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self._Slider_yNoise.setMinimum(0.0)
+        self._Slider_yNoise.setMaximum(150.0)
+        self._Slider_yNoise.setValue(50.0)
+        self._Slider_yNoise.setTickInterval(1.0)
+        self._Slider_yNoise.valueChanged.connect(self.updateYNoiseStd)
+        self._filter.setYNoiseStd(50.0)
+
+        # Layout for Noise
+        self._NoiseCtrlLayout = QtGui.QVBoxLayout()
+        self._NoiseCtrlLayout.addWidget(self._Label_xNoise)
+        self._NoiseCtrlLayout.addWidget(self._Label_xNoiseVal)
+        self._NoiseCtrlLayout.addWidget(self._Slider_xNoise)
+        self._NoiseCtrlLayout.addWidget(self._Label_yNoise)
+        self._NoiseCtrlLayout.addWidget(self._Label_yNoiseVal)
+        self._NoiseCtrlLayout.addWidget(self._Slider_yNoise)
+
+        '''
+        Parameters
+        '''
+
+        self._Label_Params = {}
+        self._Grid_Params = {}
+        self._Edit_Params = {}
+        self._Layout_Params = {}
+        self._Layout_AllParams = QtGui.QHBoxLayout()
+        self._Layout_AllParams.setSpacing(20)
+
+        for p_key, p_val in self._filter.getFilterParams().iteritems():
+            
+            self._Label_Params[p_key] = QtGui.QLabel('{0:s} ({1:s})'.format(p_key, p_val.doc))
+            self._Label_Params[p_key].setAlignment(QtCore.Qt.AlignCenter)
+
+            self._Grid_Params[p_key] = QtGui.QGridLayout()
+            self._Grid_Params[p_key].setHorizontalSpacing(0)
+            self._Grid_Params[p_key].setVerticalSpacing(0)
+            self._Edit_Params[p_key] = self.createMatrix4x4InputGUI(self._Grid_Params[p_key], p_val.data)
+            
+            self._Layout_Params[p_key] = QtGui.QVBoxLayout()
+            self._Layout_Params[p_key].setSpacing(2)
+            self._Layout_Params[p_key].addWidget(self._Label_Params[p_key])
+            self._Layout_Params[p_key].addLayout(self._Grid_Params[p_key])
+
+            self._Layout_AllParams.addLayout(self._Layout_Params[p_key])
+
+        '''
+        Live Update
+        '''
+
+        '''
+        Main Layout
+        '''
 
         # Layout
-        self._GridLayout = QtGui.QGridLayout()
-        self._GridLayout.setSpacing(10)
-        self.setLayout(self._GridLayout)
+        self._ParamLayout = QtGui.QVBoxLayout()
+        self._ParamLayout.addLayout(self._HistCtrlLayout)
+        self._ParamLayout.addLayout(self._NoiseCtrlLayout)
+        self._ParamLayout.addLayout(self._UpdateCancelButtonLayout)
 
-        self._GridLayout.addWidget(self._plt.getPlot(), 0, 0)
-        self._GridLayout.addWidget(self._PushButton_update, 0, 1)
-        self._GridLayout.addWidget(self._PushButton_quit, 0, 2)
+        self._Layout = QtGui.QHBoxLayout()
+        self._Layout.addWidget(self._plt.getPlot())
+        self._Layout.addLayout(self._ParamLayout)
+
+        self._LayoutMain = QtGui.QVBoxLayout()
+        self._LayoutMain.addLayout(self._Layout)
+        self._LayoutMain.addLayout(self._Layout_AllParams)
+
+        self.setLayout(self._LayoutMain)
 
         # Show widget
         self.show()
+    
+    def createMatrix4x4InputGUI(self, GridLayout, ParamData):
+        paramCellEdit = {}
+        for i in range(4):
+            for j in range(4):
+                paramCellEdit[(i, j)] = QtGui.QLineEdit()
+                paramCellEdit[(i, j)].setValidator(QtGui.QDoubleValidator())
+                paramCellEdit[(i, j)].setText(str(ParamData[i, j]))
+                GridLayout.addWidget(paramCellEdit[(i, j)], i, j)
 
-    def _updateParam(self):
-        print '{:50s}'.format(datetime.now().time().strftime('%m/%d/%y %I:%M:%S %p') + ' Updated Paramters')
+        return paramCellEdit
 
-    def _quitApp(self):
-        print '{:50s}'.format('Exit application')
+    def stopThread(self):
+        self._filter_updater.stop()
+        self._thread.quit()
+        self._thread.wait()
+
+    def updateParams(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Updated Paramters')
+
+        for p_key, p_val in self._Edit_Params.iteritems():
+            for 
+            
+            self._filter.setFilterParams()
+
+    def quitApp(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Exit application')
         self.stopThread()
         QtCore.QCoreApplication.instance().quit()
+
+    def updateMeasHistLen(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Updated Measurement History Length to {0:d}'.format(self._SpinBox_measHistLen.value()))
+        self._filter.setMeasHistLen(self._SpinBox_measHistLen.value())
+
+    def updatePredHistLen(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Updated Prediction History Length to {0}'.format(self._SpinBox_predHistLen.value()))
+        self._filter.setPredHistLen(self._SpinBox_predHistLen.value())
+
+    def updateXNoiseStd(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Updated X Noise Std to {0}'.format(self._Slider_xNoise.value()))
+        self._Label_xNoiseVal.setText('{:.1f}'.format(self._Slider_xNoise.value()))
+        self._filter.setXNoiseStd(self._Slider_xNoise.value())
+
+    def updateYNoiseStd(self):
+        print '{:50s}\n'.format(datetime.now().strftime('%m/%d/%y %I:%M:%S%p') + ' Updated Y Noise Std to {0}'.format(self._Slider_yNoise.value()))
+        self._Label_yNoiseVal.setText('{:.1f}'.format(self._Slider_yNoise.value()))
+        self._filter.setYNoiseStd(self._Slider_yNoise.value())
 
 def main():
 
@@ -260,7 +475,7 @@ def main():
     plt = Plot()
     ex = App(plt, fltr)
 
-    proxy = pg.SignalProxy(plt.getPlot().scene().sigMouseMoved, delay=0, rateLimit=REFRESH_RATE, slot=plt.mousePosUpdate)
+    proxy = pg.SignalProxy(plt.getPlot().scene().sigMouseMoved, delay=0, rateLimit=1000.0, slot=plt.mousePosUpdate)
 
     sys.exit(app.exec_())
 
